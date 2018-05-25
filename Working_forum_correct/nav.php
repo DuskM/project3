747c82db9c808fa3eb106beef3658e134ba4cfa5
Dennis ten Hoove <dennistenhoove@hotmail.com>
2018-05-25 10:34:00 +0200

    Removed underline in navigation, in the correct fine this time

--- Working_forum_correct/nav.php
+++ Working_forum_correct/nav.php
@@ -18,13 +18,11 @@
         <img id="forum_banner" src="">  <!-- Hier de link voor de forumbanner invullen -->
     </header>
     <nav>  <!-- Dit zijn de knoppen voor het navigatie lint  -->
-        <a class="nav_button" href="index.php?pag=home">Home</a>
-        <a class="nav_button" href="index.php?pag=TEST">TEST</a>
-        <a class="nav_button" href="index.php?pag=forum">Forum</a>
-        <a class="nav_button" href="index.php?pag=registreren">Registreren</a>
-        <a class="nav_button" href="index.php?pag=inloggen">Login</a>
-        <a class="nav_button" href="index.php?pag=profiel">Profiel</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=home">Home</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=TEST">TEST</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=forum">Forum</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=registreren">Registreren</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=inloggen">Login</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=profiel">Profiel</a>
     </nav>
 </div>
-
-
