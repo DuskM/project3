d76b97e26fe970a38ff631755a4308a4064ea076
Unknown <m.koning02@student.alfa-college.nl>
2018-06-07 16:18:48 +0200

    nav vergeten

--- forum/nav.php
+++ forum/nav.php
@@ -19,9 +19,7 @@
     </header>
     <nav>  <!-- Dit zijn de knoppen voor het navigatie lint  -->
         <a class="nav_button" style="text-decoration:none" href="index.php?pag=home">Home</a>
-        <a class="nav_button" style="text-decoration:none" href="index.php?pag=TEST">TEST</a>
-        <a class="nav_button" style="text-decoration:none" href="index.php?pag=forum">Forum</a>
-        <a class="nav_button" style="text-decoration:none" href="index.php?pag=registreren">Registreren</a>
+        <a class="nav_button" style="text-decoration:none" href="index.php?pag=topic">Topic</a>
         <a class="nav_button" style="text-decoration:none" href="index.php?pag=inloggen">Login</a>
         <a class="nav_button" style="text-decoration:none" href="index.php?pag=profiel">Profiel</a>
     </nav>
