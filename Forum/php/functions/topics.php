962371bc5f4d7d1da6370149ce81d4ffa39f8464
Unknown <m.koning02@student.alfa-college.nl>
2018-11-13 15:51:59 +0100

    oops

--- Forum/php/functions/topics.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: marvi
- * Date: 09/11/2018
- * Time: 12:26
- */
--- Forum/php/sessions.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: marvi
- * Date: 13/11/2018
- * Time: 14:56
- */
